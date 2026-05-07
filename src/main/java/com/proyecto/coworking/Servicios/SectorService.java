80292c6534e43eac31bc44f00ea3cee3a4856744
LaraSperanza <larusp8@gmail.com>
2026-05-07 00:41:10 -0300

    Add: sector service con funcion para obtener ubicación de sala

---
+++ src/main/java/com/proyecto/coworking/Servicios/SectorService.java
@@ -0,0 +1,35 @@
+package com.proyecto.coworking.Servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.proyecto.coworking.Modelos.DTOs.SectorCompletoDTO;
+import com.proyecto.coworking.Modelos.DTOs.SectorDTO;
+import com.proyecto.coworking.Modelos.Entidades.Sala;
+import com.proyecto.coworking.Modelos.Entidades.Sector;
+import com.proyecto.coworking.Repositorios.SectorRepository;
+
+@Service
+public class SectorService {
+    
+    private final SectorRepository sectorRepository;
+
+    public SectorService(SectorRepository sectorRepository){
+        this.sectorRepository = sectorRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public String obtenerUbicacionCompleta(Long idSector){
+        List<String> sectoresSala = new ArrayList<>();
+        Sector padre = sectorRepository.findById(idSector).get();
+        while (padre != null) {
+            sectoresSala.add(padre.getNombre());
+            padre = padre.getSectorPadre();
+        }
+        return String.join(" > ", sectoresSala);
+    }
+
+}
