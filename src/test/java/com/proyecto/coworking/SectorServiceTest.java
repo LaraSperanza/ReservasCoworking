987d29a6bd29f1238364bcaad8b298100b257d9a
LaraSperanza <larusp8@gmail.com>
2026-05-08 15:13:34 -0300

    test: modificación de nombres

--- src/test/java/com/proyecto/coworking/SectorServiceTest.java
+++ src/test/java/com/proyecto/coworking/SectorServiceTest.java
@@ -1,26 +1,23 @@
 package com.proyecto.coworking;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.math.BigDecimal;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.proyecto.coworking.Servicios.SectorService;
 
 @SpringBootTest
 @Transactional
 public class SectorServiceTest {
+    
     @Autowired 
     private SectorService sectorService;
 
     @Test
-    public void obtenerUbicacionCompleta(){
+    @DisplayName("Test para obtener la ubicación completa de un sector")
+    public void obtenerUbicacionCompletaTest(){
         String ubicacionActual = sectorService.obtenerUbicacionCompleta(4L);
         String ubicacionEsperada = "Ala Norte > Piso 1 > Edificio Central";
         assertEquals(ubicacionEsperada, ubicacionActual);
